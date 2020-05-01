f8daeca04b1b77cf2d39800a34d30f70a40a22e1
Phil Middlemass <philm400@gmail.com>
2020-05-01 16:30:21 +0000

    New image base and Lap blocker when clock is stopped

--- public/js/timer-worker.js
+++ public/js/timer-worker.js
@@ -1,9 +1,11 @@
 var timeStart;
 var timer;
 var raceTimer = 0;
+var running = false;
 
 self.onmessage = function(e) {
     if (e.data.function == 'start') {
+        running = true;
         timeStart = performance.now();
         splitsL1 = [{raceTime: 0, lapSplit: 0}];
         timer = setInterval(() => {
@@ -12,13 +14,14 @@ self.onmessage = function(e) {
             self.postMessage({function: 'timer', value: raceTimer});
         }, 3)
     }
-    if (e.data.function == 'lap') {
+    if ((e.data.function == 'lap') && (running)) {
         var prevLapTime = splitsL1[splitsL1.length - 1].raceTime;
         var lap = raceTimer - prevLapTime;
         splitsL1.push({raceTime: raceTimer, lapSplit: lap}); 
         self.postMessage({function: 'lap', value: lap, raceTime:raceTimer, lane: e.data.lane, num: splitsL1.length - 1});
     }
     if (e.data.function == 'stop') {
+        running = false;
         splitsL1 = [{raceTime: 0, lapSplit: 0}];
         clearInterval(timer);
     }
